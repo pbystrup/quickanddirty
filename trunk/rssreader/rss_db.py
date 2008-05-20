e30a0d899cb6927729e825f9fa3ff9623f63a848
juhapekka.piiroinen <juhapekka.piiroinen@1ba2575c-4b41-0410-bbe8-1710a195d073>
2008-05-20 21:48:00 +0000

    changed news topics sorting

--- trunk/rssreader/rss_db.py
+++ trunk/rssreader/rss_db.py
@@ -33,19 +33,20 @@ class db_connection(object):
 		self.close()
 		
 	def default_db(self):
-		self.cursor.execute("create table config (feed,time,title)")
-		self.cursor.execute("create table feeds (time,title,link,feedid)")
+		self.cursor.execute("create table rssfeeds (feed,time,title)")
+		self.cursor.execute("create table rsstopics (time,title,link,feedid)")
 #		self.insert_new_feed()
 		self.insert_new_feed(feedurl="http://rss.slashdot.org/Slashdot/slashdot",title="Slashdot")
 		self.insert_new_feed(feedurl="http://feeds.feedburner.com/ItechNewsNet",title="iTech News Net")
 		self.insert_new_feed(feedurl="http://www.linuxdevices.com/backend/headlines.rss",title="Linux Devices")
+		self.insert_new_feed(feedurl="http://feeds.engadget.com/weblogsinc/engadget",title="Engadget")
 		
 	def close(self):
 		self.conn.commit()
 		self.cursor.close()
 		
 	def insert_new_feed(self,feedurl="http://www.iltalehti.fi/rss/rss.xml",title="Iltalehti"):
-		self.cursor.execute("insert into config(feed,time,title) values ('"+feedurl+"',date('now'),'"+title+"')")
+		self.cursor.execute("insert into rssfeeds(feed,time,title) values ('"+feedurl+"',datetime('now'),'"+title+"')")
 		
 	def read_feeds(self,table):
 		
@@ -57,11 +58,11 @@ class db_connection(object):
 		return self.answer
 	
 	def read_feedname(self,feedid):
-		self.cursor.execute("select title from config where rowid="+str(feedid))
+		self.cursor.execute("select title from rssfeeds where rowid="+str(feedid))
 		return self.cursor.fetchone()[0]
 		
 	def read_newslinks(self):
-		self.cursor.execute("select link from feeds")
+		self.cursor.execute("select link from rsstopics")
 		self.answer2 = self.cursor.fetchall()
 		self.answer = []
 		for item in self.answer2:
@@ -69,26 +70,26 @@ class db_connection(object):
 		return self.answer
 		
 	def read_news(self):
-		self.cursor.execute("select * from feeds order by time")
+		self.cursor.execute("select * from rsstopics order by time desc")
 		self.answer = self.cursor.fetchall()
 		return self.answer		
 	
 	def write_table(self,title,link,feedid):
-		self.cursor.execute("select * from feeds where title = ? and link = ?",(title,link))
+		self.cursor.execute("select * from rsstopics where title = ? and link = ?",(title,link))
 		self.aa = self.cursor.fetchone()
 		try:
 			if len(self.aa) > 0: pass
 			else:
-				self.cursor.execute("insert into feeds values (date('now'),?,?,?)",(title,link,feedid))
+				self.cursor.execute("insert into rsstopics values (datetime('now'),?,?,?)",(title,link,feedid))
 				self.conn.commit()
 		except TypeError:
-			self.cursor.execute("insert into feeds values (date('now'),?,?,?)",(title,link,feedid))
+			self.cursor.execute("insert into rsstopics values (datetime('now'),?,?,?)",(title,link,feedid))
 			self.conn.commit()
 
 	def write_stamp_feed(self,feed):
-		self.cursor.execute("update config set time=date('now') where feed=?",(feed,))
+		self.cursor.execute("update rssfeeds set time=datetime('now') where feed=?",(feed,))
 		self.conn.commit()
 		
 	def write_stamp_news(self,title,link):
-		self.cursor.execute("update feeds set time=date('now') where title=?",(title,))
+		self.cursor.execute("update rsstopics set time=datetime('now') where title=?",(title,))
 		self.conn.commit()
