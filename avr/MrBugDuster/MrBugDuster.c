20b6a0c6c7f1167f350c08f95f1f6074e711e4fd
juhapekka.piiroinen <juhapekka.piiroinen@1ba2575c-4b41-0410-bbe8-1710a195d073>
2010-03-07 13:30:22 +0000

    includes now pwm control for 2 parallax continuous rotation servos and uses PD3 and PD4 as direction controls thru INT0 and INT1.
    -- just committed to store the current process. not doing all the things what is wanted and the code is not commented, etc.

--- avr/MrBugDuster/MrBugDuster.c
+++ avr/MrBugDuster/MrBugDuster.c
@@ -1,4 +1,4 @@
-#define F_CPU 8000000
+#define F_CPU 8000000UL
 
 #include <avr/io.h>          
 #include <stdbool.h> 
@@ -7,6 +7,7 @@
 #include <util/delay.h> 
 #include <avr/interrupt.h> 
 
+int direction = 0;
 
 #define _TOP 2480
 
@@ -19,14 +20,35 @@
 #define _STOP_LEFT (_TOP/20*1.5)
 #define _STOP_RIGHT (_TOP/20*1.5)
 
+ISR(INT0_vect) 
+{
+   	OCR1A = _BACKWARD_LEFT; 
+   	OCR1B = _BACKWARD_RIGHT;
+}
+
+ISR(INT1_vect) 
+{
+   	OCR1A = _STOP_LEFT; 
+   	OCR1B = _STOP_RIGHT;
+}
+
+
 int main (void) 
 { 
-   DDRB |= (1<<PB4)|(1<<PB3); 
+   PCMSK |= (1<<PIND2)|(1<<PIND3);
+   MCUCR = (1<<ISC01) | (1<<ISC00); 
+   GIMSK  |= (1<<INT0)|(1<<INT1);
+
+   DDRB ^= (1<<PB4)|(1<<PB3); 
+
    ICR1 = _TOP; 
    OCR1A = _STOP_LEFT; 
    OCR1B = _STOP_RIGHT; 
    TCCR1A = (1<<COM1A1)|(1<<COM1B1)|(1<<WGM11); 
    TCCR1B = (1<<WGM13)|(1<<WGM12)|(1<<CS11);
+
+   sei();
+
 	while (1) {
 
 	}
