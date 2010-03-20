76a21b8f171980d4808b5ea7a43533960976ce7e
juhapekka.piiroinen <juhapekka.piiroinen@1ba2575c-4b41-0410-bbe8-1710a195d073>
2010-03-20 12:05:13 +0000

    added first draft of the jpiiAVR

---
+++ trunk/jpiiavr/mainwindow.h
@@ -0,0 +1,60 @@
+/*******************************************************************************
+  Application name: jpiiAVR
+  Filename: mainwindow.h
+  Author: Juhapekka Piiroinen
+  License: GNU/GPLv3
+  Description:
+      This is a quick-and-dirty application for avrdude.
+
+  (C) 2010 Juhapekka Piiroinen
+  http://code.google.com/p/quickanddirty
+  juhapekka.piiroinen@gmail.com
+*******************************************************************************/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtGui/QMainWindow>
+#include <QFileDialog>
+#include <QProcess>
+#include <QSettings>
+
+#define AVRDUDE "/usr/bin/avrdude"
+
+namespace Ui
+{
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+protected slots:
+    void handleBrowseFlash();
+    void handleWriteFlash();
+    void handleTestConnection();
+    void handleAvrDudeStdout();
+    void handleAvrDudeFinished(int);
+    void handleAvrDudeStarted();
+    void handleBrowseFlashRead();
+    void handleReadFlash();
+    void restoreSettings();
+    void saveSettings();
+    void loadLicense();
+
+protected:
+    void connectComponents();
+    void fillComboboxes();
+
+private:
+    Ui::MainWindow *ui;
+    QProcess* avrDude;
+    QSettings settings;
+};
+
+#endif // MAINWINDOW_H
