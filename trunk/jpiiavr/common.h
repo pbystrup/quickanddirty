35c71878a791fb28b04f0b7da8d42a0fe58126d6
juhapekka.piiroinen <juhapekka.piiroinen@1ba2575c-4b41-0410-bbe8-1710a195d073>
2010-03-21 18:02:28 +0000

    added timestamps, filename and linenumber to debug messages moved Help tab from Flash tab to independent tab

--- trunk/jpiiavr/common.h
+++ trunk/jpiiavr/common.h
@@ -2,8 +2,9 @@
 #define COMMON_H
 
 #include <QDebug>
+#include <QDateTime>
 
-#define DIN qDebug() << ">>" << __FUNCTION__;
-#define DOUT qDebug() << "<<" << __FUNCTION__;
+#define DIN qDebug() << QDateTime::currentDateTime().toString("dd.MM.yyyy hh:mm:ss.zzz") << ">>" << __FILE__ << __LINE__ << __FUNCTION__;
+#define DOUT qDebug() << QDateTime::currentDateTime().toString("dd.MM.yyyy hh:mm:ss.zzz") << "<<" << __FILE__ << __LINE__ << __FUNCTION__;
 
 #endif // COMMON_H
