a33c06d7e77802676c3a6fbadfeafd5eecd2f9bc
juhapekka.piiroinen <juhapekka.piiroinen@1ba2575c-4b41-0410-bbe8-1710a195d073>
2010-03-21 16:06:31 +0000

    added automatic settings saving on closeevent added syntax highter to source code editor

--- trunk/jpiiavr/codeeditor.h
+++ trunk/jpiiavr/codeeditor.h
@@ -4,6 +4,8 @@
 #include <QPlainTextEdit>
 #include <QObject>
 
+#include "highlighter.h"
+
 class QPaintEvent;
 class QResizeEvent;
 class QSize;
@@ -32,6 +34,7 @@ private slots:
 
 private:
  QWidget *lineNumberArea;
+ Highlighter* highlighter;
 };
 
 
